6a1755e2ca54cb78f743387a3565a3aef12889ae
Cecilia Russano <cecistudy23@gmail.com>
2025-08-27 16:42:43 -0300

    Update Cont_Bancaria_Terminal.java
    Upadate do Codigo basico de uma Conta Bancaria no Terminal

--- Projetos_DioJava/Cont_Bancaria_Terminal.java
+++ Projetos_DioJava/Cont_Bancaria_Terminal.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class SimuContBancaria {
     public static void main(String[] args) {
-        System.out.println(" ");
-    }
+		        Scanner sc = new Scanner(System.in);
+
+		        int numeroConta;
+		        String nome;
+		        double saldo;
+		        double deposito;
+		        double retDinheiro = 0.0;
+
+		        System.out.print("Numero da Conta: ");
+		        numeroConta = sc.nextInt();
+		        sc.nextLine(); 
+
+    		        System.out.print("Nome Titular: ");
+    		        nome = sc.nextLine();
+
+		        System.out.print("Saldo da conta: R$ ");
+		        saldo = sc.nextDouble();
+
+    		        System.out.print("Valor do Depósito: R$ ");
+    		        deposito = sc.nextDouble();
+    		        saldo += deposito;
+
+		        sc.nextLine(); 
+		        System.out.print("Irá Retirar um valor da Conta (S/N)?\n ");
+		        String s_n = sc.nextLine().trim();
+		        
+		            System.out.print("\n___Cadastro de Conta___\n"); 
+		        
+		        if (s_n.equalsIgnoreCase("S")) {
+		            System.out.print("Valor a retirar: R$ \n");
+		            retDinheiro = sc.nextDouble();
+		            saldo -= retDinheiro;
+		            
+		           
+		            if (saldo < 0) {
+		                System.out.println("Atenção: saldo negativo após a retirada.");
+		            }
+		          
+		            System.out.printf( "\nNumero da Conta: %d\nNome Titular: %s\nSaldo da Conta Total: R$ %.2f\nValor Depositado: R$ %.2f\nValor Retirado: R$ %.2f\n",
+		                numeroConta, nome, saldo, deposito, retDinheiro
+		            );
+		        } else {
+		            System.out.printf("Conta: %d\nNome Titular: %s\nSaldo da Conta Total: R$ %.2f\nValor do Depositado: R$ %.2f\n",
+		                numeroConta, nome, saldo, deposito
+		            );
+		        }
+	}
+	
 }
