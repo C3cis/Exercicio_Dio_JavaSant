de890384f20b77208a69ce034929eafc22e27362
Cecilia Russano <cecistudy23@gmail.com>
2025-08-27 18:45:16 -0300

    Update Identificador_unico.java
    Update do Desafio #1 de Identificador Único

--- Projetos_DioJava/ Sintaxe_Java/Identificador_unico.java
+++ Projetos_DioJava/ Sintaxe_Java/Identificador_unico.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+import java.util.UUID;
+
 public class IdentificadorUnico {
+
     public static void main(String[] args) {
-        System.out.println(" ");
+		 
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Digite seu nome: ");
+	        String nome = scanner.nextLine();
+
+	        int numero = 0;
+	        boolean numeroValido = false;
+
+	        while (!numeroValido) {
+	            System.out.print("Digite o ID ");
+	            String NumeroRemov = scanner.nextLine().replaceAll(" ", "");
+
+	            try {
+	                numero = Integer.parseInt(NumeroRemov);
+	                numeroValido = true;
+	            } catch (NumberFormatException e) {
+	                System.out.println("ID inválido!");
+	            }
+	        }
+
+	        String nomeFormatado = nome.toLowerCase().replaceAll(" ", "");
+	        String identificador = nomeFormatado + "-" + numero;
+
+	        System.out.println("\nIdentificador Unico Gerado: " + identificador);
+        }
     }
-}
