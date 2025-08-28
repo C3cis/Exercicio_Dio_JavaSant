a72c45f505e258c6475fad6ef578ab41ffdc0bd2
Cecilia Russano <cecistudy23@gmail.com>
2025-08-28 20:07:07 -0300

    Create ConnectionFactory.java

---
+++ Projetos_DioJava/BoardTarefas/util/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    // ajuste URL / user / password 
+    private static final String URL = "jdbc:mysql://localhost:3306/board_db?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "senha";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
