23ba946668131105dfa1d31749e19d5837731bc9
Cecilia Russano <cecistudy23@gmail.com>
2025-08-28 22:04:57 -0300

    Create Class_Iphone.java

---
+++ Projetos_DioJava/Model_iPhone.UML/Class_Iphone.java
@@ -0,0 +1,45 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String modelo;
+    private String cor;
+
+    public Iphone(String modelo, String cor) {
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+}
