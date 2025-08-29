5a953d46ecdfe6d0d401351be73329b20ddd0b3b
Cecilia Russano <cecistudy23@gmail.com>
2025-08-28 22:05:44 -0300

    Create Main_UML.java

---
+++ Projetos_DioJava/Model_iPhone.UML/Main_UML.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone("iPhone 14", "Preto");
+
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        meuIphone.ligar("11987654321");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
